e6fbc6647c4c7dca9de85af487482635722ade33
webifya <161525559+webifya@users.noreply.github.com>
2026-07-28 01:29:04 +0600

    Add includes/class-webform.php

---
+++ includes/class-webform.php
@@ -0,0 +1,59 @@
+<?php
+
+defined('ABSPATH') || exit;
+
+final class Webform {
+    private static $instance;
+
+    public static function instance() {
+        if (!self::$instance) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    private function __construct() {
+        require_once WEBFORM_PATH . 'includes/class-webform-admin.php';
+        require_once WEBFORM_PATH . 'includes/class-webform-public.php';
+
+        add_action('init', array($this, 'register_post_types'));
+        add_action('plugins_loaded', array($this, 'load_textdomain'));
+        new Webform_Admin();
+        new Webform_Public();
+    }
+
+    public function load_textdomain() {
+        load_plugin_textdomain('webform', false, dirname(plugin_basename(WEBFORM_FILE)) . '/languages');
+    }
+
+    public function register_post_types() {
+        register_post_type('webform_form', array(
+            'labels' => array(
+                'name' => __('Webforms', 'webform'),
+                'singular_name' => __('Webform', 'webform'),
+            ),
+            'public' => false,
+            'show_ui' => false,
+            'supports' => array('title'),
+            'capability_type' => 'post',
+            'map_meta_cap' => true,
+        ));
+
+        register_post_type('webform_entry', array(
+            'labels' => array(
+                'name' => __('Entries', 'webform'),
+                'singular_name' => __('Entry', 'webform'),
+            ),
+            'public' => false,
+            'show_ui' => false,
+            'supports' => array('title'),
+            'capability_type' => 'post',
+            'map_meta_cap' => true,
+        ));
+    }
+
+    public static function activate() {
+        self::instance()->register_post_types();
+        flush_rewrite_rules();
+    }
+}
