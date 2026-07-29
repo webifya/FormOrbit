35930dabdea6db43f6e90660edb7a171db838a32
webifya <161525559+webifya@users.noreply.github.com>
2026-07-29 22:05:35 +0600

    Brand FormOrbit plugin files

---
+++ includes/class-formorbit.php
@@ -0,0 +1,57 @@
+<?php
+
+defined('ABSPATH') || exit;
+
+final class Webform {
+    private static $instance;
+
+    public static function instance() {
+        if (!self::$instance) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    private function __construct() {
+        require_once WEBFORM_PATH . 'includes/class-formorbit-admin.php';
+        require_once WEBFORM_PATH . 'includes/class-formorbit-public.php';
+        require_once WEBFORM_PATH . 'includes/class-formorbit-mailer.php';
+
+        add_action('init', array($this, 'register_post_types'));
+        new Webform_Admin();
+        new Webform_Public();
+        new Webform_Mailer();
+        do_action('webform_loaded', $this);
+    }
+
+    public function register_post_types() {
+        register_post_type('webform_form', array(
+            'labels' => array(
+                'name' => __('FormOrbit', 'formorbit'),
+                'singular_name' => __('FormOrbit Form', 'formorbit'),
+            ),
+            'public' => false,
+            'show_ui' => false,
+            'supports' => array('title'),
+            'capability_type' => 'post',
+            'map_meta_cap' => true,
+        ));
+
+        register_post_type('webform_entry', array(
+            'labels' => array(
+                'name' => __('Entries', 'formorbit'),
+                'singular_name' => __('Entry', 'formorbit'),
+            ),
+            'public' => false,
+            'show_ui' => false,
+            'supports' => array('title'),
+            'capability_type' => 'post',
+            'map_meta_cap' => true,
+        ));
+    }
+
+    public static function activate() {
+        self::instance()->register_post_types();
+        flush_rewrite_rules();
+    }
+}
