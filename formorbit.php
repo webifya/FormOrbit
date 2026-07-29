fe7e2e7df51c5e92fff8fcb78f55f146b7d61742
webifya <161525559+webifya@users.noreply.github.com>
2026-07-30 00:32:08 +0600

    Release FormOrbit 4.4.0 live container builder

--- formorbit.php
+++ formorbit.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: FormOrbit
  * Description: A visual drag-and-drop, multi-step form builder for WordPress.
- * Version: 4.3.1
+ * Version: 4.4.0
  * Author: Mahfuzar Rahman
  * Author URI: https://profiles.wordpress.org/mahfuzar/
  * License: GPLv2 or later
@@ -18,7 +18,7 @@ if (defined('WEBFORM_FILE')) {
     return;
 }
 
-define('WEBFORM_VERSION', '4.3.1');
+define('WEBFORM_VERSION', '4.4.0');
 define('WEBFORM_EDITION', 'free');
 define('WEBFORM_FILE', __FILE__);
 define('WEBFORM_PATH', plugin_dir_path(__FILE__));
