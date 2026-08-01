a68bbc82f063babf1af14c22271a511d8027a9c3
Mahfuzar Rahman <161525559+webifya@users.noreply.github.com>
2026-08-01 19:24:49 +0600

    Fix Free style preview icon alignment and release 4.9.4

--- formorbit.php
+++ formorbit.php
@@ -3,7 +3,7 @@
  * Plugin Name: FormOrbit
  * Plugin URI: https://www.webninjallc.com/plugins/formorbit/
  * Description: Build responsive contact, multi-step, survey, quiz and registration forms with visual editing, templates, SMTP, analytics and secure entries.
- * Version: 4.9.3
+ * Version: 4.9.4
  * Author: Mahfuzar Rahman
  * Author URI: https://profiles.wordpress.org/mahfuzar/
  * License: GPLv2 or later
@@ -19,7 +19,7 @@ if (defined('WEBFORM_FILE')) {
     return;
 }
 
-define('WEBFORM_VERSION', '4.9.3');
+define('WEBFORM_VERSION', '4.9.4');
 define('WEBFORM_EDITION', 'free');
 define('WEBFORM_FILE', __FILE__);
 define('WEBFORM_PATH', plugin_dir_path(__FILE__));
